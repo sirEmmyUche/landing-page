0506698b83f73680fcdc4e32f757c28f7fbb9c4a
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2022-04-12 10:12:27 +0100

    changing the script code

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const menuBar = () => {
+    let burger = document.getElementById("burger");
+    let nav = document.querySelectorAll(".nav-links");
+    burger.addEventListener("click",() => {
+        // loop over since its a node collections of an array
+        for(let i =0; i < nav.length; i++) {
+             nav[i].classList.toggle('responsive');
+        }
+
+        burger.classList.toggle('toggle');
+    })
+
+}
+
+
+menuBar()
+
+
+
+/*
 const navSlide =  () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
