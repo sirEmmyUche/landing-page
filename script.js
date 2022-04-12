caa4e23e66e0675b15f1089207d8a7bcddb2153f
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2022-04-12 10:58:56 +0100

    modified

--- script.js
+++ script.js
@@ -2,45 +2,15 @@ const menuBar = () => {
     let burger = document.getElementById("burger");
     let nav = document.querySelectorAll(".nav-links");
     burger.addEventListener("click",() => {
-        // loop over since its a node collections of an array
+        // loop over since its a node collection of an array
         for(let i =0; i < nav.length; i++) {
              nav[i].classList.toggle('responsive');
         }
-
-        burger.classList.toggle('toggle');
     })
 
 }
 
-
 menuBar()
 
+//burger.classList.toggle('toggle');
 
-
-/*
-const navSlide =  () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navlinks = document.querySelectorAll('.nav-links li');
-	
-	burger.addEventListener('click', ()=>{ 
-	//toggle nav
-		nav.classList.toggle('nav-active');
-		
-		//animate links
-	navlinks.forEach((link,index) =>{
-		if (link.style.animation) {
-			link.style.animation = ''
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
-		}
-	});
-	//burger animation
-	burger.classList.toggle('toggle');
-
-});
-
-
-}
-
-navSlide();
